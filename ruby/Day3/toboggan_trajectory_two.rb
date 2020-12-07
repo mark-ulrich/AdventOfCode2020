3b6790be63c6471bc331d76c48c5e6d6d92374a2
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-12-07 16:01:12 -0600

    ruby: Day 3 Part 2

--- ruby/Day3/toboggan_trajectory_two.rb
+++ ruby/Day3/toboggan_trajectory_two.rb
@@ -1,9 +1,16 @@
 #!/usr/bin/env ruby
 
-tree_count = 0
+counts = Array.new(5, 0)
 STDIN.readlines.map(&:chomp).each_with_index do |line, i|
-  offset = (i * 3) % line.size
-  tree_count += 1 if line[offset] == '#'
+  counts[0] += 1 if line[i % line.size] == '#'
+  counts[1] += 1 if line[(i * 3) % line.size] == '#'
+  counts[2] += 1 if line[(i * 5) % line.size] == '#'
+  counts[3] += 1 if line[(i * 7) % line.size] == '#'
+  counts[4] += 1 if i.even? && line[(i / 2) % line.size] == '#'
 end
 
-puts "Tree count: #{tree_count}"
+product = counts.reduce do |prod, val|
+  prod *= val
+  prod
+end
+puts "Product: #{product}"
